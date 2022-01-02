7006d1bf8f91760a906cf31eb03fc4a5d22cd8a8
jba <julien.bille@gmail.com>
2022-01-02 15:17:36 +0100

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.julienba/super-rss "0.1.0-SNAPSHOT"
+(defproject com.github.julienba/super-rss "0.1.0"
   :description "A library to read RSS feed"
   :url "http://github.com/julienba/super-rss"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
